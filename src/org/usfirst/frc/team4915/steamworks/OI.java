4b241074ed97f4a31a40e9d640b94f669c1c7033
dbadb <dana.batali@gmail.com>
2017-01-19 13:55:23 -0800

    Support for build versioning and timestamping (untested).
    Copied basic structure from phroa's 2016 solution.
    As stated in wpibuild.xml, we needed to clone the 2017 wpi build support
    rather than attempt to use 2016's.
    
    Verified that the correct data arrives into the manifest file.
    Have not yet verified that data makes it to smart dashboard.

--- src/org/usfirst/frc/team4915/steamworks/OI.java
+++ src/org/usfirst/frc/team4915/steamworks/OI.java
@@ -7,6 +7,10 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.io.InputStream;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+import java.io.IOException;
 
 public class OI
 {
@@ -18,20 +22,36 @@ public class OI
 
     private Robot m_robot;
     private SendableChooser<Command> m_chooser;
+    private Logger m_logger;
 
     public OI(Robot robot)
     {
         m_robot = robot;
+		m_logger = new Logger("OI", Logger.Level.DEBUG);
         initAutoOI();
         initDrivetrainOI();
         initIntakeOI();
         initLauncherOI();
         initClimberOI();
-    }
-    
-    public Command getAutoCommand()
-    {
-        return m_chooser.getSelected();
+
+        /* VERSION STRING!!  */
+        try (InputStream manifest = getClass().getClassLoader().
+                                        getResourceAsStream("META-INF/MANIFEST.MF"))
+        {
+            // build a version string
+            Attributes attributes = new Manifest(manifest).getMainAttributes();
+            String buildStr = "by: " + attributes.getValue("Built-By") +
+                              "  on: " + attributes.getValue("Built-At") +
+                              "  vers:" + attributes.getValue("Code-Version");
+            SmartDashboard.putString("Build", buildStr);
+            m_logger.notice("Build " + buildStr);;
+        }
+        catch (IOException e)
+        {
+            SmartDashboard.putString("Build", "version not found!");
+            m_logger.error("Build version not found!");;
+            m_logger.exception(e, true /*no stack trace needed*/);
+        }
     }
     
     private void initAutoOI()
@@ -43,6 +63,11 @@ public class OI
         SmartDashboard.putData("Auto mode", m_chooser);
     }
     
+    public Command getAutoCommand()
+    {
+        return m_chooser.getSelected();
+    }
+   
     private void initDrivetrainOI()
     {
         m_robot.getDrivetrain().setDriveStick(m_driveStick);
