04e78128a265da55469221ee965b2bd0782ca9b0
pietroglyph <declanfreemangleason@gmail.com>
2017-02-19 18:31:50 -0800

    Fixed modulo operator.

--- src/org/usfirst/frc/team4915/steamworks/commands/groups/ParameterizedCommandGroup.java
+++ src/org/usfirst/frc/team4915/steamworks/commands/groups/ParameterizedCommandGroup.java
@@ -22,7 +22,7 @@ public class ParameterizedCommandGroup extends CommandGroup
     {
         int safeMaxIndex = 0;
         requires(drivetrain);
-        if (params.length%2 != 1) 
+        if (params.length%2 != 0)
         {
             // We subtract 2 here because arrays start at 0 in Java, and we want one less then the greatest array index so we subtract one more
             safeMaxIndex = params.length-2; // If we have an odd number of indices, then the safe length is one less so we don't get a null array value
