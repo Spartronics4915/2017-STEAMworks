106bf47e83699154c7bb81b605aa1a9da7cc2529
Noah <2013islandboy@gmail.com>
2017-02-20 13:08:15 -0800

    Changed Intake Speed

--- src/org/usfirst/frc/team4915/steamworks/subsystems/Intake.java
+++ src/org/usfirst/frc/team4915/steamworks/subsystems/Intake.java
@@ -18,7 +18,7 @@ public class Intake extends SpartronicsSubsystem
         REVERSE
     }
 
-    private static final double INTAKE_SPEED = 0.75;
+    private static final double INTAKE_SPEED = 0.90;
 
     private CANTalon m_intakeMotor;
 
