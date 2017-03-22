2a48ad8d600a7a99824e0d131ab181876cbd004b
NPruen <niklas.pruen@gmail.com>
2017-03-21 17:36:01 -0700

    Drive shoot and deliver gear command group added

--- src/org/usfirst/frc/team4915/steamworks/OI.java
+++ src/org/usfirst/frc/team4915/steamworks/OI.java
@@ -21,6 +21,7 @@ import org.usfirst.frc.team4915.steamworks.commands.ReverseArcadeDriveCommand;
 import org.usfirst.frc.team4915.steamworks.commands.groups.ParameterizedCommandGroup;
 import org.usfirst.frc.team4915.steamworks.subsystems.Climber;
 import org.usfirst.frc.team4915.steamworks.subsystems.Drivetrain;
+import org.usfirst.frc.team4915.steamworks.subsystems.Intake;
 import org.usfirst.frc.team4915.steamworks.subsystems.Intake.State;
 import org.usfirst.frc.team4915.steamworks.subsystems.Launcher;
 import org.usfirst.frc.team4915.steamworks.subsystems.Launcher.LauncherState;
@@ -205,6 +206,7 @@ public class OI
         display.add("Preset: Place Gear Position 2");
         display.add("Preset: Drive and Shoot Position 3");
         display.add("Preset: Drive Shoot and Cross Baseline Position 3 with Curve");
+        display.add("Preset: Drive Shoot and deliver Gear Position 3");
 
         display.addAll(m_autoReplayOptions);
 
@@ -263,6 +265,15 @@ public class OI
                             "Drive Timeout", "" + (37 + returnForSide(m_alliance, 0, -3)), "2.5",
                             "Shoot",
                             "Curve", "-125", "0.5");
+                    break;
+                case "Drive Shoot and deliver Gear Position 3":
+                    result = new ParameterizedCommandGroup(drivetrain, launcher, this,
+                            "Drive", "" + (-42 + returnForSide(m_alliance, 0, 10)),
+                            "Turn", "-45",
+                            "Drive Timeout", "" + (37 + returnForSide(m_alliance, 0, -3)), "2.5",
+                            "Shoot",
+                            "Curve", "" + (-105 + returnForSide(m_alliance, 0, -7)), "" + returnForSide(m_alliance, 0.32, 0.38), // different values for alining
+                            "Turn", "-60");
                     break;
                 default:
                     break;
